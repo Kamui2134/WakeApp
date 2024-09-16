ab55cc0850ad2f0646e8402ae66d0ec65d5c72e3
Kamui2134 <ctorkey@gmail.com>
2024-09-16 20:51:40 +0300

    Header is done

---
+++ src/components/Header.vue
@@ -0,0 +1,89 @@
+<script setup>
+import logo from '@/assets/logo.svg'
+</script>
+
+<template>
+  <header class="header">
+    <div class="container">
+      <img :src="logo" alt="logo" width="48" height="48" />
+      <nav class="nav">
+        <a class="nav-link" href="#">Games</a>
+        <a class="nav-link" href="#">Contact Us</a>
+        <a class="nav-link" href="#">Terms of Use</a>
+        <a class="nav-link" href="#">About</a>
+      </nav>
+      <div class="authorization-buttons">
+        <a class="sign-up" href="#">Sign up</a>
+        <a class="button log-in" href="#">Log in</a>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="css" scoped>
+.header {
+  background: var(--color-translucent);
+  position: fixed;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+.header .container {
+  height: 72px;
+  padding-inline: 200px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+}
+.nav {
+  display: flex;
+  align-items: center;
+  margin-left: 64px;
+  height: 100%;
+  gap: 36px;
+}
+.nav-link {
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  text-align: center;
+  font-size: var(--fontSize-16);
+  font-weight: var(--weight-bold);
+  color: var(--color-white);
+}
+.nav-link:hover {
+  color: transparent;
+  -webkit-background-clip: text;
+  background-clip: text;
+  background-image: var(--color-gradient);
+}
+.nav-link:hover::after {
+  position: absolute;
+  bottom: 0;
+  right: 50%;
+  transform: translate(50%);
+  content: '';
+  width: 50px;
+  height: 4px;
+  background: var(--color-gradient);
+  clip-path: var(--clip-path-bottom-line);
+}
+.authorization-buttons {
+  margin-inline-start: auto;
+  display: flex;
+  gap: 32px;
+}
+.sign-up {
+  font-size: var(--fontSize-18);
+  font-weight: var(--weight-bold);
+  color: var(--color-white);
+  display: flex;
+  align-items: center;
+}
+.button.log-in {
+  width: 114px;
+  height: 40px;
+}
+</style>
---
+++ src/pages/MainPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <main>
+    <div class="container">
+      
+    </div>
+  </main>
+  <footer>
+    <div class="container"></div>
+  </footer>
+</template>
+
+<script setup></script>
+
+<style lang="css" scoped></style>
